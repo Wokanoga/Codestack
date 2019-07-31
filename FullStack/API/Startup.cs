65ddc489cdc5a157e46c97b6d7dec06715e30ac1
Wokanoga <wokanoga@gmail.com>
2019-07-31 15:30:23 -0700

    Update

--- FullStack/API/Startup.cs
+++ FullStack/API/Startup.cs
@@ -28,7 +28,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<APIContext> (options => options.UseSqlServer(Configuration.GetConnectionString("DemoDatabase")));
+            services.AddDbContext<APIContext> (options => options.UseSqlServer(Configuration.GetConnectionString("FullStackDataBase")));
             services.AddScoped<UserService>();
             services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
         }
