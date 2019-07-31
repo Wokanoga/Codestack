d658f53cb247be4e83c87ec1730f5f3a7b1baf08
Wokanoga <wokanoga@gmail.com>
2019-07-31 11:37:52 -0700

    Entities

--- FullStack/API/Entities/User.cs
+++ FullStack/API/Entities/User.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace API.Models 
+namespace API.Entities
 {
 
 	public class User 
--- FullStack/API/Startup.cs
+++ FullStack/API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -25,7 +26,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<APIContext> (options => options.UseSqlServer(Configuration.GetConnectionString("DemoDatabase")));
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
